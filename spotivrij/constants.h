ba4bacac5cf9848234acb6509a9a489add3ebc83
Sebastiaan Hoejenbos <seb@Sebastiaans-MacBook-Pro-2.local>
2014-08-12 11:33:47 +0200

    - added constants.h: this is where we will store all constants, it is included in every file using -Prefix.pch

---
+++ spotivrij/constants.h
@@ -0,0 +1,18 @@
+#ifdef CONFIGURATION_Debug
+#define baseURL @"http://machine.local:3000"
+#else
+#define baseURL @"https://url.production.com"
+#endif
+
+#define backendUsername @"username"
+#define backendPassword @"password"
+
+#define officeIdentifier @"com.office.location"
+#define officeBeaconIdentifier @"com.office.iBeacon"
+#define keychainID @"com.weareastronaut.spottivrij"
+
+#define beaconUUID @"xxx"
+
+#define geoFenceLong 4.384977
+#define geoFenceLat 51.997868
+#define geoFenceRadius 30.0
